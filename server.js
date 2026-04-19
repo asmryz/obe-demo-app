3dc674fcd723feb5ec300cfe03fc6adde877d4f0
Asim Riaz <asim.riaz@live.com>
2026-04-19 07:37:57 +0500

    deploy v0.1

--- server.js
+++ server.js
@@ -6,8 +6,11 @@ import indexRouter from './routes/index.js';
 const app = express();
 const PORT = process.env.PORT || 5001;
 
+
 app.use(cors());
 app.use(express.json());
+// Serve static files from dist
+app.use(express.static('dist'));
 app.use('/api', indexRouter);
 
 
